fab8185e468827cc10fca99a1afa65132c291cac
snoman786 <syedmohd.noman@gmail.com>
2020-08-16 12:27:22 +0530

    Added Logo and NavigationBar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => {
 
     return (
     <header className = {classes.Toolbar}>
         <div>Menu</div>
-        <div>Logo</div>
+        <Logo/>
         <nav>
-            ....?????
+            <NavigationItems/>
         </nav>
     </header>
 
